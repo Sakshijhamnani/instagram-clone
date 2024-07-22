692148e299f2362c99f9f14551285074b84db184
Sakshijhamnani <sakshijhamnani@gmail.com>
2024-07-22 12:29:16 +0530

    updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
-import { Box, Container, Flex } from '@chakra-ui/react'
+import { Box, Container, Flex,useBreakpointValue } from '@chakra-ui/react'
 import React from 'react'
 import FeedPosts from '../../Components/FeedPosts/FeedPosts'
 import SuggestedUsers from '../../Components/SuggestedUsers/SuggestedUsers'
 
 const HomePage = () => {
+  const isMobile = useBreakpointValue({ base: true, md: false })
   return (
     <Container maxW={"container.lg"}>
       <Flex gap={20}>
@@ -14,10 +15,12 @@ const HomePage = () => {
           <FeedPosts/>
         </Box>
         <Box flex={3} mr={20} display={{base:'none',lg:'block'}} maxW={"300px"}  >
-        <SuggestedUsers/>
+      { !isMobile && <SuggestedUsers/>}
         </Box>
 
       </Flex>
+
+      {isMobile && <SuggestedUsers/>}
        
     </Container>
   )
